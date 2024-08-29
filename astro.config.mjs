58c59750b1fdf8e05acc35c8d0b52eb18e0746a4
John Felix Agda <john.agda@gmail.com>
2024-08-29 18:26:27 +0800

    Updated spacing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://blkhnd-dev.github.io"
+    site: "https://blkhnd-dev.github.io",
+    prefetch: true
 });
